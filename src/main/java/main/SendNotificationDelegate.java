a526ead78a30ce973f3a6c08c9df7ab32ef0546b
metanyarius <lemuryaka@gmail.com>
2019-05-28 23:00:18 +0300

    Fixed error

--- src/main/java/main/SendNotificationDelegate.java
+++ src/main/java/main/SendNotificationDelegate.java
@@ -1,4 +1,4 @@
-package org.camunda.bpm.spring.boot.example.webapp.delegate;
+package main;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
